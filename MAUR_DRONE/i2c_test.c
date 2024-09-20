cb6a02b7c208072bfb00bffe5ef4bc8976634b8d
Maurice <otienom@lafayette.edu>
2024-09-20 17:58:48 -0400

    added 12c test module

---
+++ MAUR_DRONE/i2c_test.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "i2c.h"
+//DEFINE THE I2C ADDDRESS OF THE DEVICE TO COMMUNICATE WITH 
+#define I2C_ADDRESS 0X20
+//DEFINE A STRUCTRURE TO HOLD INFO TO SEND 
+
+int main(){
+
+
+
+//initialize i2c communication
+init_i2c(I2C_ADDRESS);
+//start i2c communication
+start_i2c();
+//write data to i2c device
+while(
+while(Node))
+
+return 0;
+
+}
