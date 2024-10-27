70db08271e2628e0a2b3e4680a83f9bb0a26b546
Maurice <otienomaurice12340@gmail.com>
2024-10-27 19:58:00 -0400

    worked on fsm

--- New folder/CALCULATOR/calculator_fsm.c
+++ New folder/CALCULATOR/calculator_fsm.c
@@ -6,38 +6,30 @@
 #include "global.h"
 #include "touchin_validation.h"
 #include "display.h"
-// //instatiate the global state variables
-//   int32_t _global_opperand1 ; 
-//   int32_t _global_opperand2 ; 
-//   char  _global_operator;
-// //initialize global state dsiplay
-//   char _global_display[50] = "|";
 //initializes the calculator fsm
-//period of button press
-
 void init_calculator_fsm(){
-    state = INITIAL_STATE;
-    operand_pressed = false;
-    equal_pressed = false;
-    clear_pressed = false;
-    error = false;
-    operator_first_press_before_operand = true;
-    negative_operand = false;
-    clear_all();
+     state = INITIAL_STATE;
+     operand_pressed = false;
+     equal_pressed = false;
+     clear_pressed = false;
+     input_operand2_complete;
+     operator_first_press_before_operand = true;
+     second_operator_pressed_after_operand2 false;
+     negative_operand = false;
+     clear_all();
+     state = INITIAL_STATE;
 }
-
 //advance the fsm by one cycle 
 void tick_calculator_fsm(){
-    //get the required operands and operators
-
-    //do the reading stuff and setting required variables
+   calc.operand1 =_global_opperand1;
+   calc.operand2 = _global_opperand2;
+   calc.operator = _global_operator;
     switch (state)
     {
     case INITIAL_STATE:
              operand_pressed = false;
              equal_pressed = false;
              clear_pressed = false;
-             error = false;
              operator_first_press_before_operand = true;
              negative_operand = false;
         //clear the global variables setting them to zero
@@ -49,73 +41,93 @@ void tick_calculator_fsm(){
                  state = INITIAL_STATE;
              else 
                  state = INPUT_OPERAND1;
-     break;
+                 break;
 
     case INPUT_OPERAND1:
                  display_text();
+                 if(input_operand1_complete){
+                 state = INPUT_OPERATOR;
+                 }
+                 else{
                  //this will either update the operands , the operator , or set clear pressed or equals pressed
                  char temp = button_info_fill();
-                update_Display(temp);
-        //check for negative
-        if(temp == '-' && operator_first_press_before_operand)
-                 {
                  update_Display(temp);
+                 //check for negative
+                 if(temp == '-' && operator_first_press_before_operand)
+                 {
                  operator_pressed = false;
                  operator_first_press_before_operand = false;
                  clear_global_operator();
                  negative_operand = true;
                  }
-         //if button fill returns an operator, set the input to completed
-        //advance  to the operator state
-         if (operator_pressed){
-             if(negative_operand){
-                  int32_t negate_operand1 = (0 - _global_opperand1);
-                  set_global_operand1(negate_operand1);
-                }
-                  input_operand1_complete = true;
-                  state = INPUT_OPERATOR;
+                 //if button fill returns an operator, set the input to completed
+                  //advance  to the operator state 
+                if (operator_pressed){
+                         if(negative_operand){
+                            int32_t negate_operand1 = (0 - _global_opperand1);
+                             set_global_operand1(negate_operand1);
+                         }
+                           input_operand1_complete = true;
+                           state = INPUT_OPERATOR;
        }
          else {
                  state = INPUT_OPERAND1;
        }
+                 }
      break;
    
-    case INPUT_OPERATOR:
-     char temp = button_info_fill();
-     //UPDATE DISPLAY
-     update_Display(temp);
-     display_text();
-    if( operator_pressed && input_operand1_complete ){
-    state = INPUT_OPERAND2;
-    }
-    else state = INPUT_OPERATOR;
+     case INPUT_OPERATOR:
+          display_text();
+          operator_pressed = false;
+          input_operand1_complete = false;
+          state = INPUT_OPERAND2;
          break;
      case INPUT_OPERAND2:
-       char temp = button_info_fill();
+         display_text();
+         char temp = button_info_fill();
        if(clear_pressed){
          state = INITIAL_STATE;
+        }
+        else if(operator_pressed){
+         second_operator_pressed_after_operand2 = true;
+         input_operand1_complete = true;
+         state = RUN_CALCULATION;
         }
        else  if (equal_pressed){
 
         state = RUN_CALCULATION;
       }
          break;
-    case RUN_CALCULATION:
-    display_text();
-//double check that equals pressed
-    if(equal_pressed){
-      equals(&calc);
-    }
+     case RUN_CALCULATION:
+     display_text();
+     if(second_operator_pressed_after_operand2){
+        equals(&calc);
         if(calc.hasError)
-    {
+           {
         //if error occurs, set the display to result
-    set_Display_Error(calc.display);
-    state = ERROR_STATE;
-    }
-     else {
-        //update the display to display result
-    set_Display_Result(calc.result);
-    state = INPUT_OPERAND1;
+     set_Display_Error(calc.display);
+     state = ERROR_STATE;
+     }
+     else{
+          set_global_operand1(calc.result);
+          second_operator_pressed_after_operand2 = false;
+          state =INPUT_OPERAND1;
+         }
+     }
+     //double check that equals pressed
+     else if(equal_pressed){
+      equals(&calc);
+     }
+        if(calc.hasError)
+     {
+        //if error occurs, set the display to result
+     set_Display_Error(calc.display);
+     state = ERROR_STATE;
+     }
+     else { 
+        //update the display string to result
+     set_Display_Result(calc.result);
+     state = INPUT_OPERAND1;
 }
          break;
     case ERROR_STATE:
--- New folder/CALCULATOR/calculator_fsm.h
+++ New folder/CALCULATOR/calculator_fsm.h
@@ -2,10 +2,20 @@
 #define CALCULATOR_FSM.C
 #include <stdbool.h>
 #include <stdlib.h>
-#include "global.h"
 //This fsm is responsible for handling th e states of the calculation
   //initialize calculator with desired values
- static Calculator calc = {_global_opperand1,_global_opperand2,_global_operator,0,_global_display,false};
+  //true if operand1 is complete
+ static bool input_operand1_complete;
+//
+static bool input_operand2_complete;
+ //checks if the - operator haas been pressed before an operand denoting a negative inpu
+ static bool operator_first_press_before_operand;
+ //set true if the keyed operand is negative
+ //second operator pressed after second operand
+ static bool second_operator_pressed_after_operand2;
+ static bool negative_operand;
+ //
+  static Calculator calc ; 
 //THERE ARE FIVE STATES FOR THE CALCULATOR
 typedef enum { INITIAL_STATE, 
         INPUT_OPERAND1,
